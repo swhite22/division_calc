f4af8c82547d4dab8259531e6f5853a9b3ff5dd3
Steve White <swhite22@student.waubonsee.edu>
2019-03-12 11:34:07 -0500

    added new feature of remainder and products

--- division_calc.py
+++ division_calc.py
@@ -1,10 +1,22 @@
 num1 = 0
 num2 = 0
 quotient = 0
+remainder = 0
+product = 0
 
-num1 = int(input('input your first number: '))
-num2 = int(input('input your second number: '))
+num1 = int(input('Input your first number: '))
+num2 = int(input('Input your second number: '))
 
 quotient = num1//num2
 
 print('your quotient is: ', quotient)
+
+num1 = int(input('Please input another, new, number: '))
+num2 = int(input('Please input another, new, number: '))
+
+quotient = num1//num2
+remainder = num1%num2
+product = num1*num2
+
+
+print('Your quotient is: ', quotient, ', with a remainder of: ', remainder, '. Your prodcut is product: ', product)
