b521f8bf4365f8a3482c954b9823970b34213187
Steve White <swhite22@student.waubonsee.edu>
2019-03-12 11:18:23 -0500

    added initial file

---
+++ division_calc.py
@@ -0,0 +1,10 @@
+num1 = 0
+num2 = 0
+quotient = 0
+
+num1 = int(input('input your first number: '))
+num2 = int(input('input your second number: '))
+
+quotient = num1//num2
+
+print('your quotient is: ', quotient)
